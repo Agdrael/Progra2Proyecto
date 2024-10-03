f7ec7d50e5d40cf9289304ae017548fa0ef00cd1
GersonSteven <gersonchachaguamolina@gmail.com>
2024-10-03 14:58:33 -0600

    Añade funcionalidad basica
    	Se agrega funcionalidad de conexion con SQL Server mediante EF.

---
+++ ProyectoProgramacion2/ProyectoProgramacion2/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoProgramacion2.Models;
+using System.Reflection;
+
+namespace ProyectoProgramacion2.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(user.Email, user.Password, user.Nombre, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    // Handle successful login
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    // Handle two-factor authentication case
+                }
+                if (result.IsLockedOut)
+                {
+                    // Handle lockout scenario
+                }
+                else
+                {
+                    // Handle failure
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(model);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ ProyectoProgramacion2/ProyectoProgramacion2/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations; //To implemenst action of valitacion.
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ProyectoProgramacion2.Models
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+
+        public string? Nombre { get; set; }
+
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}(\.[A-Za-z]{2,4})?"), Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
